0cc1a8f3efdb7cab0cb8a1ab3ac6b2cbfe5b4754
Isaac Gama <ikettee@gmail.com>
2024-01-08 22:00:08 -0800

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Implementation of the 'BaseGeometry' class with the 'area' method:
+"""
+
+'''
+File_Name: 6-base_geometry.py
+Project Title: 0x0A-python-inheritance
+'''
+
+
+class BaseGeometry:
+    """
+    # Write a class BaseGeometry (based on 5-base_geometry.py).
+    # Exception with the message area() is not implemented.....
+    # VARIABLE(" "):
+    # Base Geometry(area): Improve Geometry
+    # Return: Always 0. (Success)
+    """
+    """
+    The 'BaseGeometry' class now includes the 'area' method, which raises...
+    an exception with the message 'area() is not implemented'"""
+    def area(self):
+        raise Exception("area() is not implemented")
---
+++ 0x0A-python-inheritance/tests/README.md
@@ -0,0 +1,1 @@
+test for Python - Inheritance

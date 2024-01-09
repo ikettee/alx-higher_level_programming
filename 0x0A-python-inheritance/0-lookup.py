3a964104798ab3d96e48348b92b7aa62ef5a2782
Isaac Gama <ikettee@gmail.com>
2024-01-08 22:12:04 -0800

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-lookup = __import__('0-lookup').lookup
+"""This function returns a list of attributes and methods of an object
+"""
 
-class MyClass1(object):
-    pass
+'''
+File_Name: 0-lookup.py
+Project Title: 0x0A-python-inheritance
+'''
 
-class MyClass2(object):
-    my_attr1 = 3
-    def my_meth(self):
-        pass
 
-print(lookup(MyClass1))
-print(lookup(MyClass2))
-print(lookup(int))
+def lookup(obj):
+    """
+    # Write a function that returns the list of available attributes and....
+    # .....methods of an object:
+    # VARIABLE(" "):
+    # LookUp(obj): Lookup
+    # Return: a 'list' object.
+    """
+    """ This function takes an object as a parameter and returns the list...
+    of attributes and methods using the 'dir()' function..."""
+    return dir(obj)
